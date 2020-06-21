11a62ae602b3a8eaa276029a5c1a2a46ac5742a2
Jatin Kamboj <kumar.jatin873@gmail.com>
2020-06-21 15:34:20 +0530

    Routes Added

---
+++ src/views/Pages/backgroundTab/backgroundtab.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class backgroundtab extends Component {
+    render() {
+        return (
+            <div>
+                from BackGround Tab!!
+            </div>
+        )
+    }
+}
+
+export default backgroundtab
