a356cba860cd84e912521907927755cec9131582
Jatin Kamboj <kumar.jatin873@gmail.com>
2020-06-21 13:22:18 +0530

    Updates

---
+++ precache-manifest.cbfa98327fb6447d15e56d47f84dba98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357b36adbad2233430acfc85cb8b5821",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7e7ca077c2a30062b886",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3cf610bb3d580212bc7f",
+    "url": "/portfolio/static/js/2.9862d705.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.9862d705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7ca077c2a30062b886",
+    "url": "/portfolio/static/js/main.666fd1b3.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
