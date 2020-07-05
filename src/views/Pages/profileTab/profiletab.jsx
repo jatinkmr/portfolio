95321cb41ee684b3ddf8ec584d073d6f7c93a379
Jatin Kamboj <kumar.jatin873@gmail.com>
2020-07-05 16:41:37 +0530

    All Work Experiences Added

--- src/views/Pages/profileTab/profiletab.jsx
+++ src/views/Pages/profileTab/profiletab.jsx
@@ -76,6 +76,94 @@ class ProfileTab extends Component {
                             <p>&#x007B;</p>
                         </div>
                         <div className='experienceData'>
+                            <div className='position a'>Intern Full-Stack Web Developer</div>
+                            <div className='companyAndYear'>
+                                <div className='companyName a'>
+                                    <a
+                                        href='https://www.momkidcare.com/'
+                                        target='_blank'
+                                        rel='noopener noreferrer'
+                                        data-toggle='tooltip'
+                                        title='MomKidCare'
+                                    >
+                                        MomKidCare
+                                    </a>
+                                </div>
+                                <div className='year a'>Mar 2020 - Present</div>
+                            </div>
+                            <div className='companyWork'>
+                                <ul className='workList'>
+                                    <li>
+                                        Work on Company Product and Provide Full-Stack Developer Support.
+                                    </li>
+                                    <li>
+                                        Tools: NodeJS, ReactJS, MongoDB and Pug.
+                                    </li>
+                                    <li className='projectName'>
+                                        Project:{' '}
+                                        <a
+                                            href='https://www.momkidcare.com/'
+                                            target='_blank'
+                                            rel='noopener noreferrer'
+                                            data-toggle='tooltip'
+                                            title='MomKidCare'
+                                        >
+                                            MomKidCare
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='row workExperience'>
+                        <div className='braces'>
+                            <p>&#x007B;</p>
+                        </div>
+                        <div className='experienceData'>
+                            <div className='position a'>Intern Back-End Developer</div>
+                            <div className='companyAndYear'>
+                                <div className='companyName a'>
+                                    <a
+                                        href='https://www.entrancezone.com/'
+                                        target='_blank'
+                                        rel='noopener noreferrer'
+                                        data-toggle='tooltip'
+                                        title='EntranceZone'
+                                    >
+                                        EntranceZone
+                                    </a>
+                                </div>
+                                <div className='year a'>Nov 2019</div>
+                            </div>
+                            <div className='companyWork'>
+                                <ul className='workList'>
+                                    <li>
+                                        Work on Company Product and Provide BackEnd support in making APIs(Application Programming Interfaces).
+                                    </li>
+                                    <li>
+                                        Tools: NodeJS and PostgreSQL.
+                                    </li>
+                                    <li className='projectName'>
+                                        Project:{' '}
+                                        <a
+                                            href='https://www.entrancezone.com/'
+                                            target='_blank'
+                                            rel='noopener noreferrer'
+                                            data-toggle='tooltip'
+                                            title='EntranceZone'
+                                        >
+                                            EntranceZone
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='row workExperience'>
+                        <div className='braces'>
+                            <p>&#x007B;</p>
+                        </div>
+                        <div className='experienceData'>
                             <div className='position a'>Intern Software Developer</div>
                             <div className='companyAndYear'>
                                 <div className='companyName a'>
@@ -90,6 +178,28 @@ class ProfileTab extends Component {
                                     </a>
                                 </div>
                                 <div className='year a'>Jul 2019 - Oct 2019</div>
+                            </div>
+                            <div className='companyWork'>
+                                <ul className='workList'>
+                                    <li>
+                                        Provide BackEnd support on Companies Projects.
+                                    </li>
+                                    <li>
+                                        Tools: Core php, wordPress and MySQL.
+                                    </li>
+                                    <li className='projectName'>
+                                        Project:{' '}
+                                        <a
+                                            href='https://learning4impact.in/'
+                                            target='_blank'
+                                            rel='noopener noreferrer'
+                                            data-toggle='tooltip'
+                                            title='learning4impact'
+                                        >
+                                            Learning4Impact
+                                        </a>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
