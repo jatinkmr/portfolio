9e8ade1a525b81fe528c9c1b3f05c41a1f4a4e2d
Jatin Kamboj <kumar.jatin873@gmail.com>
2020-07-13 13:06:35 +0530

    ABout Me AddeD

--- src/views/Pages/aboutTab/aboutTab.jsx
+++ src/views/Pages/aboutTab/aboutTab.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import './aboutTab.css';
+import React, { Component } from 'react'
+import './aboutTab.css'
 
 // class AboutTab
 class AboutTab extends Component {
@@ -8,13 +8,35 @@ class AboutTab extends Component {
             <div className='row mainAboutTab fade-in'>
                 <div className='waveHand'>
                     <h1>
-                        Hi There...!! <span className='wave' role='img' aria-label='waving-hand'>👋</span>
+                        Hi There...!!{' '}
+                        <span className='wave' role='img' aria-label='waving-hand'>
+                            👋
+            </span>
                     </h1>
                 </div>
+                <div className='mainAboutHello'>
+                    I&#39;m <span>Jatin Kumar</span> from Delhi, India which is also known
+          as Capital of India. I&#39;m a Web Developer.
+          <br />
+                    <br />
+          Currently I&#39;m working as an Intern FullStack Web Developer at{' '}
+                    <span>
+                        <a
+                            href='https://www.momkidcare.com/'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            data-toggle='tooltip'
+                            title='MomKidCare'
+                        >
+                            MomKidCare
+            </a>
+                    </span>
+          . I always tried to learn new technologies in Web and try to implement them too.
+        </div>
             </div>
         )
     }
 }
 
 // export aboutTab Page
-export default AboutTab;
+export default AboutTab
