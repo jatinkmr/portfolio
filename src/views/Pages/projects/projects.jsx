75a550da6e157afdd78a803f6f8b1961aa0fb44a
Jatin Kamboj <kumar.jatin873@gmail.com>
2020-07-14 09:23:27 +0530

    Project Tab Added in navigation

---
+++ src/views/Pages/projects/projects.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './projects.css'
+
+//Class projectTab
+
+class ProjectTab extends Component {
+    render() {
+        return (
+            <div>
+                Hello from Projects
+            </div>
+        )
+    }
+}
+
+
+// export class ProjectTab
+export default ProjectTab
