1f391e45d5dbbdfa7103f56ae979d7944d91ce71
Jatin Kamboj <kumar.jatin873@gmail.com>
2020-07-15 09:09:56 +0530

    Nav Bar, ProfileTab and projects heading added

--- src/views/Pages/projects/projects.jsx
+++ src/views/Pages/projects/projects.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import './projects.css'
 
 //Class projectTab
-
 class ProjectTab extends Component {
     render() {
         return (
-            <div>
-                Hello from Projects
+            <div className='row divProjectTab'>
+                <div className='projectHeading'>
+                    <p className='a'>Some Things That I&#39;ve Built</p>
+                </div>
             </div>
         )
     }
