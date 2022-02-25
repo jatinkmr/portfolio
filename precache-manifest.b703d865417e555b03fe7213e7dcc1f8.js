4f9bab59ee11b78a3aaa2480124465a9d1716676
jatinKamboj <kumar.jatin873@gmail.com>
2022-02-25 18:42:42 +0530

    Updates

---
+++ precache-manifest.b703d865417e555b03fe7213e7dcc1f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab2f1d2b4819a34bc266812ddb0eee8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0aed5040cc82f38425a6",
+    "url": "/portfolio/static/css/main.3f7ff9b3.chunk.css"
+  },
+  {
+    "revision": "d9e40625395ef97a51cf",
+    "url": "/portfolio/static/js/2.45291c88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.45291c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aed5040cc82f38425a6",
+    "url": "/portfolio/static/js/main.e40e469e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
