b693aa3c596524317597b9a4911dc2d83584d146
jatinKamboj <kumar.jatin873@gmail.com>
2024-08-24 10:54:07 +0530

    msg correction

--- src/views/Pages/contactUs/contact.jsx
+++ src/views/Pages/contactUs/contact.jsx
@@ -19,7 +19,7 @@ class ContactTab extends Component {
                     <div className='contactHeading'>
                         I&#39;m available for hire and open to any ideas of cooperation. <br />
                         <br /> My inbox is always open for Every0ne whether you want to say
-                        &quot;Hello&quot; or have any other &quot;Question&#x2753;&quot;. <br /><br /> Just mail me or connect with me at below given medium.
+                        &quot;Hello&quot; or have any other &quot;Question&#x2753;&quot;. <br /><br />
                     </div>
                     <br /><br />
                     <div className="contactHeading">
